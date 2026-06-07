6f595df690b5767a62d18a820eaa4d05a831a87a
Andrey Golovanov <55963579+andygolo@users.noreply.github.com>
2026-06-07 15:39:30 -0700

    Add: two-integer-sum - submission-7

---
+++ Data Structures & Algorithms/two-integer-sum/submission-7.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> numMap; 
+
+        for(int i = 0; i < nums.size(); i++){
+            if(numMap.find(target - nums[i]) != numMap.end()){
+                return {numMap[target - nums[i]], i}; 
+            }
+            //numMap.insert({nums[i]})
+            numMap[nums[i]] = i; 
+        }
+
+        return {-1,-1};
+    }
+};
